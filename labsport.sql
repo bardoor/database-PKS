25e6de5d46f6ba5a1ccdcc880f55304cb647437e
unknown <garmashkir09@mail.ru>
2024-09-18 18:50:59 +0300

    добавил таблицу SportProduct

---
+++ labsport.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `SportProduct` (
+	`id` INTseschool_01_work6seschool_01_work6SportProduct,
+	`sport_product_name` VARCHAR(50),
+	`date` DATE,
+	`information` VARCHAR(50),
+	`size` FLOAT,
+	`count` INT
+);
